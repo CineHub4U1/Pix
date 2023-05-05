349807dc98b86b75e2bf9eb224b28284be1ff3cb
CineHub4U1 <106883661+CineHub4U1@users.noreply.github.com>
2023-05-05 23:38:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,14 @@ import os
 import pixeldrain
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import BOT_TOKEN, API_ID, API_HASH
-from telethon import TelegramClient
 
 
-client = TelegramClient(
+Bot = Client(
     "Pixeldrain-Bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
 )
-
 
 @Bot.on_message(filters.private & filters.command("start"))
 async def start(bot, update):
